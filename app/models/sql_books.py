3f0775b82a12f52cb221f1aefa03b527bf861a70
Dave Hocker <AtHomeX10@gmail.com>
2016-07-13 10:20:49 -0500

    Improve Sql injection prevention on url parameters

--- app/models/sql_books.py
+++ app/models/sql_books.py
@@ -20,14 +20,6 @@ from app.database.connection import get_db, get_cursor
 from types import *
 
 def get_books_by_page(page, pagesize, sort_col, sort_dir):
-    # q = db_session.query(Book).join(Collaborations, Collaborations.book_id==Book.id)
-    # q = q.join(Series, Series.id==Book.series_id)
-    # q = append_order_by_clause(q, sort_col, sort_dir)
-    # count = q.count();
-    # books = q.offset(page * pagesize).limit(pagesize)
-    # dict_books = books_todict(books)
-    # return {"rows": dict_books, "count": count}
-
     # Column sorting
     # TODO There are consistency issues. Collab records with no book or no author.
     # We use left outer joins here to get all books regardless of collaborations status.
@@ -40,65 +32,33 @@ def get_books_by_page(page, pagesize, sort_col, sort_dir):
         s.name as Series from books as b
         left outer join collaborations as c on c.book_id=b.id
         left outer join authors as a on a.id=c.author_id
-        left join series as s on s.id=b.series_id
+        left outer join series as s on s.id=b.series_id
         order by {0}
         limit :limit offset :offset
         """.format(get_sort_clause(sort_col, sort_dir))
-    inputs = {"limit": pagesize, "offset": int(page * pagesize)}
+    parameters = {"limit": pagesize, "offset": int(page * pagesize)}
 
     csr = get_cursor()
-    rst = csr.execute(stmt, inputs)
+    rst = csr.execute(stmt, parameters)
     rows = rows2list(rst)
 
-    return {"rows": rows, "count": get_all_books_count()}
-
-def get_all_books_count():
-    """
-    Total count of books
-    :return:
-    """
-    stmt = """
-        select count(*) from books as b
-        left outer join collaborations as c on c.book_id=b.id
-        left outer join authors as a on a.id=c.author_id
-        left join series as s on s.id=b.series_id
-        """
-    csr = get_cursor()
-    count = csr.execute(stmt).fetchone()[0]
-    return count
+    return {"rows": rows, "count": get_filtered_books_count(None, None, None)}
 
 def search_for_books_by_page(page, pagesize, author_id, series_id, search_arg, sort_col, sort_dir):
-    # q = db_session.query(Book).join(Collaborations, Collaborations.book_id==Book.id)
-    # q = q.join(Author, Author.id==Collaborations.author_id)
-    # q = q.join(Series, Series.id==Book.series_id)
-    # if author_id:
-    #     # This appears to be slow, but it works for finding an author's books
-    #     # q = q.filter(Book.authors.any(Author.id==author_id))
-    #     # This technique appears to be much faster
-    #     q = q.filter(Book.series_id==series_id)
-    # elif series_id:
-    #     q = q.filter(Book.series_id==series_id)
-    # if search_arg:
-    #     s = "%" + search_arg + "%"
-    #     q = q.filter(Book.Title.like(s))
-    #
-    # count = q.count()
-    # books = q.order_by(func.lower(Book.Title)).offset(page * page_size).limit(page_size)
-    # dict_books = books_todict(books)
-    #
-    # return {"rows": dict_books, "count": count}
-
     # Build where clause based on filter inputs
-    inputs = {"limit": pagesize, "offset": int(page * pagesize)}
+    # We used named parameters to prevent Sql injection vulnerabilities
+    parameters = {"limit": pagesize, "offset": int(page * pagesize)}
     wh = ""
     if author_id:
-        wh = "where c.author_id=" + str(author_id)
+        wh = "where c.author_id=:author_id"
+        parameters["author_id"] = str(int(author_id))
     elif series_id:
-        wh = "where b.series_id=" + str(series_id)
+        wh = "where b.series_id=:series_id"
+        parameters["series_id"] = str(int(series_id))
     if search_arg:
-        # Need Sql injection check on search arg
+        # Sql injection prevention on search arg
         s = "%" + search_arg + "%"
-        inputs["like"] = s
+        parameters["like"] = s
         wh = 'where b.Title like :like'
 
     stmt = """
@@ -117,24 +77,27 @@ def search_for_books_by_page(page, pagesize, author_id, series_id, search_arg, s
         """.format(get_sort_clause(sort_col, sort_dir), wh)
 
     csr = get_cursor()
-    rst = csr.execute(stmt, inputs)
+    rst = csr.execute(stmt, parameters)
     rows = rows2list(rst)
 
     return {"rows": rows, "count": get_filtered_books_count(author_id, series_id, search_arg)}
 
 def get_filtered_books_count(author_id, series_id, search_arg):
     # Build where clause based on filter inputs
+    # We used named parameters to prevent Sql injection vulnerabilities
     wh = ""
-    parameters = ()
+    parameters = {}
     if author_id:
-        wh = "where c.author_id=" + str(int(author_id))
+        wh = "where c.author_id=:author_id"
+        parameters["author_id"] = str(int(author_id))
     elif series_id:
-        wh = "where b.series_id=" + str(int(series_id))
+        wh = "where b.series_id=:series_id"
+        parameters["series_id"] = str(int(series_id))
     if search_arg:
-        # Need to do Sql injection check on search arg
+        # Sql injection prevention on search arg
         s = "%" + search_arg + "%"
-        parameters += (s.encode('utf-8'),)
-        wh = 'where b.Title like ?'
+        parameters["like"] = s.encode('utf-8')
+        wh = 'where b.Title like :like'
 
     stmt = """
         select count(*) from books as b
@@ -145,10 +108,7 @@ def get_filtered_books_count(author_id, series_id, search_arg):
         """.format(wh)
 
     csr = get_cursor()
-    if len(parameters) > 0:
-        count = csr.execute(stmt, parameters).fetchone()[0]
-    else:
-        count = csr.execute(stmt).fetchone()[0]
+    count = csr.execute(stmt, parameters).fetchone()[0]
     return count
 
 
@@ -171,7 +131,7 @@ def get_sort_clause(sort_col, sort_dir):
         "Author": "lower(a.LastName) {0}, lower(a.FirstName) {0}"
     }
 
-    # Sql injection check
+    # Sql injection prevention
     sd = "asc"
     if sort_dir.lower() == "desc":
         sd = "desc"
