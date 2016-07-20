917a98b8dd44156eef7b5f6b46a440e8eba5157d
Dave Hocker <AtHomeX10@gmail.com>
2016-07-20 11:03:00 -0500

    Polish up database creation program.

--- app/database/load_db.py
+++ app/database/load_db.py
@@ -2,6 +2,7 @@
 
 import os
 from sqlite3 import dbapi2 as sqlite3
+import sys
 
 dbname = 'susannas_library.sqlite3'
 
@@ -12,11 +13,11 @@ def connect_db():
     return rv
 
 
-def init_db(filename):
+def init_db(schema_script_name):
     """Initializes the database."""
     db = connect_db()
-    with open(filename , mode='r') as f:
-        print 'Creating database using script: ' + filename
+    with open(schema_script_name , mode='r') as f:
+        print 'Creating new database {0} using schema script {1}'.format(dbname, schema_script_name)
         try:
             db.cursor().executescript(f.read())
         except Exception as ex:
@@ -43,13 +44,37 @@ def seed_db(filename):
                 print "\r", line_count,
     db.commit()
     db.close()
+    print "\r", line_count
     print ""
 
 
 if __name__ == "__main__":
+    if len(sys.argv) == 2 and sys.argv[1] == '--help':
+        print ""
+        print "Create and load a fresh library database"
+        print ""
+        print "\tload_db.py [db_name]"
+        print ""
+        print "\tdb_name defaults to tables_for_sqlite3.sql"
+        print "\tAny existing database will be deleted and recreated"
+        print ""
+        exit(0)
+
+    # Resolve schema init script
+    if len(sys.argv) > 1:
+        schema_script_name = sys.argv[1]
+    else:
+        schema_script_name = "tables_for_sqlite3.sql"
+
+    # Delete existing database
+    try:
+        os.remove(dbname)
+        print "Deleted existing database"
+    except:
+        pass
 
     # Recreate the database and schema
-    init_db("tables_for_sqlite3.sql")
+    init_db(schema_script_name)
     print "Database created"
 
     # Seed database
