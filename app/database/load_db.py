b2761d19661eee83026b1fe6afb98911798cde6f
Dave Hocker <AtHomeX10@gmail.com>
2016-07-20 11:05:26 -0500

    Add license text.

--- app/database/load_db.py
+++ app/database/load_db.py
@@ -1,4 +1,19 @@
-
+#
+# Susanna's New Library
+# Copyright (C) 2016  Dave Hocker (email: AtHomeX10@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the LICENSE file for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program (the LICENSE file).  If not, see <http://www.gnu.org/licenses/>.
+#
 
 import os
 from sqlite3 import dbapi2 as sqlite3
