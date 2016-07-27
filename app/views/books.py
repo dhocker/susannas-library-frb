4db8509dfdbf7460e187fee6522d8609967f5833
Dave Hocker <AtHomeX10@gmail.com>
2016-07-27 11:29:30 -0500

    Clean up dead code from original books page.

--- app/views/books.py
+++ app/views/books.py
@@ -15,85 +15,11 @@
 # along with this program (the LICENSE file).  If not, see <http://www.gnu.org/licenses/>.
 #
 from app import app
-from flask import Flask, request, Response, session, g, redirect, url_for, abort, \
-    render_template, flash, jsonify
-from app.models.authors import get_author
-from app.models.books import get_all_books, insert_book, get_books_in_series, \
-    update_book, delete_book_by_id, search_for_books, books_todict
-from app.models.series import get_series
-from app.models.models import Author, Book
-from app.models.models import db_session
+from flask import request
+from app.models.books import insert_book, update_book, delete_book_by_id
 import logging
 
 logger = logging.getLogger("app")
-
-
-@app.route("/books-page", methods=['GET'])
-#@login_required                                 # Use of @login_required decorator
-def get_books_page():
-    # Check for author id first.
-    if "author" in request.args:
-        # Show books for author
-        id = request.args.get('author', '')
-        logger.info("Show books for author: %s", id)
-        author = get_author(id)
-        name = author.LastName + ", " + author.FirstName
-        filter_by = "author"
-    elif "series" in request.args:
-        # Show books for series
-        id = request.args.get('series', '')
-        logger.info("Show books for series: %s", id)
-        series = get_series(id)
-        name = series.name
-        filter_by = "series"
-    elif "search" in request.args:
-        # Show books by search criteria
-        id = ""
-        name = request.args.get('search', '')
-        logger.info("Show books for search criteria: %s", name)
-        filter_by = "search"
-    else:
-        # All books
-        id = ""
-        name = ""
-        filter_by = ""
-    return render_template("books.html", filter_by=filter_by, id=id, name=name)
-
-
-@app.route("/books", methods=['GET'])
-#@login_required                                 # Use of @login_required decorator
-def get_books_with_filter():
-    """
-    Get books with optional search parameters.
-    No search parameters returns all books.
-    Parameter author=author_id returns all books for an author.
-    Parameter series=series_id returns all books in a series.
-    Parameter search=search_arg returns all books where search_arg is in title.
-    Parameter sortcol=sort_column returns books sorted by this column
-    Parameter sortdir=sort_direction asc (ascending) or desc (descending)
-    :return:
-    """
-    author_id = request.args.get('author', '')
-    series_id = request.args.get('series', '')
-    search_arg = request.args.get('search', '')
-    if author_id:
-        logger.info("Books for author: %s", author_id)
-    elif series_id:
-        logger.info("Books for series: %s", series_id)
-    elif search_arg:
-        logger.info("Books containing: %s", search_arg)
-    else:
-        pass
-
-    # TODO Implement sorting
-    if author_id or series_id or search_arg:
-        books = search_for_books(author_id, series_id, search_arg)
-    else:
-        logger.info("All books")
-        books = get_all_books()
-
-    json = jsonify({'data': books})
-    return json
 
 
 @app.route("/book", methods=['POST'])
