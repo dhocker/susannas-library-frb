408f9c8b1163c2312a8455ef26849edb1f8d4989
Dave Hocker <AtHomeX10@gmail.com>
2016-08-08 15:44:10 -0500

    Resolve eslint issues

--- app/static/js/cookie-jar.js
+++ app/static/js/cookie-jar.js
@@ -5,21 +5,21 @@
 */
 
 export function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + d.toGMTString();
     document.cookie = cname + "=" + String(cvalue) + "; " + expires;
 }
 
 export function getCookieString(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') {
+    const name = cname + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
             c = c.substring(1);
         }
-        if (c.indexOf(name) == 0) {
+        if (c.indexOf(name) === 0) {
             return c.substring(name.length, c.length);
         }
     }
